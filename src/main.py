7a9dc060a98eae91eb391f367e0abfffff193acc
nazanin1369 <nazanin1427@gmail.com>
2017-02-11 12:53:51 -0800

    :pencil: Data exploration

--- src/main.py
+++ src/main.py
@@ -1,27 +1,52 @@
 import yahoo_finance as yahoo
 import pandas as pd
-##
-## Retrieves data with Yahoo finanace API and saves it in an csv format
-##
-def retrieveStockData(symbol):
-    print("Retriving data for ticker _" + symbol + "_ .....")
-    target_data = yahoo.Share(symbol).get_historical("2015-01-01", "2017-01-01")
-    target_data_df = pd.DataFrame(target_data)
-    target_data_df.to_csv("../data/" + symbol + "_stock_data.csv")
-    print("Data for ticker _" + symbol + "_ has been saved to ../data/"+ symbol + "_stock_data.csv")
 
-##
-## Read retrived stock data into a data frame
-##
-def getStockDataFrame(symbol):
-    return  pd.read_csv("./data/" + symbol + "_stock_data.csv", index_col=None, header=0, parse_dates=["Date"])
+
+###### Entry Parameters #######
+startDate = '2015-01-01'
+endDate = '2017-01-01'
+ticker = 'GOOG'
+
+#Used for re-running: stops querying the API if we already have the data
+fetchData = False
+
+#Stock Data- first step is to obtain the list of stocks, and then select a stock to run through machine learning
+fileName = 'data/stocksData.csv'
+###############################
+
+# returive stock data using yahoo Finance API and return a dataFrame
+def retrieveStockData():
+    if fetchData:
+        frames = []
+        for symbol in ['YHOO', 'GOOG', 'GPRO', 'MSFT', 'LNKD']:
+            print('Retriving data for ticker _' + symbol + '_ .....')
+            target_data = yahoo.Share(symbol).get_historical(startDate, endDate)
+            df = pd.DataFrame(target_data).sort_values(by='Date')
+            df = df[['Symbol', 'Date','Open', 'Close', 'Adj_Close', 'High','Low', 'Volume']]
+            frames.append(df)
+
+        data = pd.concat(frames)
+        # save as CSV to stop blowing up their API
+        data.to_csv(fileName, index_col=None, header=0, parse_dates=['Date'])
+    else:
+        # read the existing csv
+        data = pd.read_csv(fileName)
+
+    # Fill blank or missing data
+    data.fillna(method='ffill', inplace=True)
+    data.fillna(method='bfill', inplace=True)
+    return data
+
+
+df = retrieveStockData()
+df.head()
 
 ##
 ## Main method
 ##
 def main():
     #read the data for a specific stock
-    retrieveStockData("YHOO");
+    retrieveStockData()
     #Analyze the data
     #build graphs
     #Analyze features
